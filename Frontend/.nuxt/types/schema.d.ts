82581ee33121ac2ef8e67973b01945e7cc2fcf15
tristanhendry <147661648+tristanhendry@users.noreply.github.com>
2024-11-13 14:50:12 -0500

    a

---
+++ Frontend/.nuxt/types/schema.d.ts
@@ -0,0 +1,40 @@
+import { NuxtModule, RuntimeConfig } from 'nuxt/schema'
+declare module 'nuxt/schema' {
+  interface NuxtConfig {
+    ["kinde"]?: typeof import("@nuxtjs/kinde").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
+    ["devtools"]?: typeof import("@nuxt/devtools").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
+    ["telemetry"]?: typeof import("@nuxt/telemetry").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
+    modules?: (undefined | null | false | NuxtModule | string | [NuxtModule | string, Record<string, any>] | ["@nuxtjs/kinde", Exclude<NuxtConfig["kinde"], boolean>] | ["@nuxt/devtools", Exclude<NuxtConfig["devtools"], boolean>] | ["@nuxt/telemetry", Exclude<NuxtConfig["telemetry"], boolean>])[],
+  }
+  interface RuntimeConfig {
+   app: {
+      baseURL: string,
+
+      buildAssetsDir: string,
+
+      cdnURL: string,
+   },
+
+   kinde: {
+      authDomain: string,
+
+      clientId: string,
+
+      redirectURL: string,
+
+      logoutRedirectURL: string,
+
+      postLoginRedirectURL: string,
+
+      clientSecret: string,
+   },
+  }
+  interface PublicRuntimeConfig {
+
+  }
+}
+declare module 'vue' {
+        interface ComponentCustomProperties {
+          $config: RuntimeConfig
+        }
+      }
