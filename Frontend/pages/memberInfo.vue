6546921e7fda70c48c1764e653f982f27d2d4fed
alea-atkinson <147665827+alea-atkinson@users.noreply.github.com>
2024-11-27 11:08:13 -0500

    update potential member info page

--- Frontend/pages/memberInfo.vue
+++ Frontend/pages/memberInfo.vue
@@ -5,18 +5,19 @@
     <div class="welcome">
     
       <p class="text-display-2">
+        <br>
         Potential Member Information
       </p>
       <br>
-    </div>
-    <div class="info">
+    
+   <div class="info">
     <p>*Skill evaluations for Fall 2024 are now closed. Check back in the spring for new info session dates.</p>
-    </div>
+  
+  
     <br>
     <br>
-    <div class="welcome">
-      <div class=" indent">
-    <NuxtLink to="https://rsconnect.recsports.ufl.edu/program/GetProgramDetails?courseId=b78354d4-26b8-4a12-8ff3-af85564e3bc4"><button style="background-color:  #1243e5; color: white;   border-radius: 10px; padding: 5px; padding-left:10px; padding-right:10px"><p style= "font-size: 20px"><b>Sports Club Waiver</b></p></button></NuxtLink>
+   
+    <NuxtLink to="https://rsconnect.recsports.ufl.edu/program/GetProgramDetails?courseId=b78354d4-26b8-4a12-8ff3-af85564e3bc4"><button style="background-color:  #1243e5; color: white;   border-radius: 10px; padding: 5px; padding-left:10px; padding-right:10px;"><button:hover style="background-color: antiquewhite;"><p style= "font-size: 20px"><b>Sports Club Waiver</b></p></button:hover></button></NuxtLink>
     
         <br><br>
     
@@ -39,7 +40,7 @@
     </p>
     <br>
     <p style="text-indent: 40px">*Please arrive at the field 20 minutes before the session. </p>
-    </div>
+  </div>
     </div>
  
 </template>
@@ -47,17 +48,37 @@
 <style>
 .text-display-2{
   text-align: center;
+  color:#FA4616;
+  text-shadow:
+    -1px -1px 0 #fff,
+    1px -1px 0 #fff,
+    -1px 1px 0 #fff,
+    1px 1px 0 #fff;  
+  
 }
   .welcome{
+    align-items: center;
     text-indent: 40px;
-  }
+    background-image: url(https://static.vecteezy.com/system/resources/previews/028/622/004/non_2x/soccer-field-in-flat-style-football-stadium-illustration-on-isolated-background-sport-plan-sign-business-concept-vector.jpg);
+    background-size:cover;
+    min-height: 800px;
+}
+  
 
   .info{
     text-indent: 20px;
-
     margin-left: 20px;
     margin-right: 800px;
     font-weight: bold;
+    background-color: black;
+    min-height: 400px;
+    text-align: center;
+    color:white;
+    padding: 50px;
+    border-radius: 20px;
+    margin-left: 500px;
+    margin-right: 500px;
+    margin-top:100px;
   }
 .home{
   color:black;
@@ -65,5 +86,6 @@
 .memberInfo{
   color:#FA4616;
 }
+
 
 </style>
